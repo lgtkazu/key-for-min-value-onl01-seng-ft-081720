9ef05ec9f186a5a4ffca7fdf7083a35532ec61f6
Seth Mariscotes <seth.mariscotes@yahoo.com>
2020-08-28 23:23:44 +0000

    Automatically backed up by Learn

---
+++ key_for_min.rb
@@ -0,0 +1,14 @@
+def key_for_min_value(name_hash)
+
+  min_key = nil
+  min_value = nil
+  name_hash.each do |key, value|
+    min_key = key if min_key == nil 
+    min_value = value if min_value == nil
+    if value < min_value
+    	min_value = value
+    	min_key = key
+    end
+  end
+  min_key
+end 	
